2240e7b4cb4c438f97ec0364d73c2be8a42c4d3e
Robert Wloch <robert@rowlo.de>
2022-04-10 18:17:15 +0200

    Add jest with passing test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+  moduleFileExtensions: ["js", "ts", "json", "vue"],
+  transform: {
+    "^.+\\.(js|ts)$": "ts-jest",
+    "^.+\\.vue$": "vue-jest",
+  },
+};
