6fa135e6474c0c75b104bc4572f025382e1840e7
Robert Wloch <robert.wloch@iits-consulting.de>
2022-12-01 15:50:46 +0100

    add input parsing

--- src/aoc2022/day01.ts
+++ src/aoc2022/day01.ts
@@ -1,3 +1,43 @@
-export function findElfWithMostCalories(input: string) {
-  return []
+type Elf = {
+  calories: number[]
+  totalCalories: number,
+}
+
+type ElvesWithMaxTotalCalories = {
+  elfNumber: number,
+  elf: Elf,
+}
+function templateElf(): Elf {
+  return {
+    calories: [],
+    totalCalories: 0,
+  } 
+}
+
+export function convertInputToElves(input: string): Elf[] {
+  const inputAsArray = input ? input.split('\n') : []
+  const result: Elf[] = []
+
+  if (inputAsArray.length) {
+    let calories: number[] = []
+    inputAsArray.forEach(inputLine => {
+      if (+inputLine) {
+        calories.push(+inputLine)
+      } else {
+        result.push({calories, totalCalories: calories.reduce((sum, current) => sum + current)})
+        calories = []
+      }
+    });
+    if (calories.length > 0) {
+      result.push({calories, totalCalories: calories.reduce((sum, current) => sum + current)})
+      calories = []
+    }
+  }
+
+  return result
+}
+
+export function findElfWithMostCalories(elves: Elf[]): ElvesWithMaxTotalCalories[] {
+  const result: ElvesWithMaxTotalCalories[] = []
+  return result
 }
--- tests/unit/aoc/day01.spec.ts
+++ tests/unit/aoc/day01.spec.ts
@@ -1,10 +1,18 @@
-import {findElfWithMostCalories} from '@/aoc2022/day01'
+import {convertInputToElves, findElfWithMostCalories} from '@/aoc2022/day01'
 
 describe('day01', () => {
-  describe('puzzle 1', () => {
-    it('has gets an empty list as input and finds no elves', () => {
+  describe('convertInputToElves', () => {
+    it('gets an empty list as input and returns no elves', () => {
       const input = ''
-      expect(findElfWithMostCalories(input)).toStrictEqual([])
+      expect(convertInputToElves(input)).toStrictEqual([])
+    })
+
+    it('gets a list with one entry as input and returns one elve with that calories value', () => {
+      const input = '1000'
+      expect(convertInputToElves(input)).toStrictEqual([{calories:[1000], totalCalories: 1000}])
     })
   })
+
+  // describe('findElfWithMostCalories', () => {
+  // })
 })
