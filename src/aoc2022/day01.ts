bdeaa51cf552d246ca971d7ce622d4960ad54971
Robert Wloch <robert.wloch@iits-consulting.de>
2022-12-01 22:10:55 +0100

    refactor and rename

--- src/aoc2022/day01.ts
+++ src/aoc2022/day01.ts
@@ -1,11 +1,6 @@
 export type Elf = {
   calories: number[]
-  totalCalories: number,
-}
-
-export type ElvesWithMaxTotalCalories = {
-  elfNumber: number,
-  elf: Elf,
+  totalCalories: number
 }
 
 export function convertInputToElves(input: string): Elf[] {
@@ -14,16 +9,22 @@ export function convertInputToElves(input: string): Elf[] {
 
   if (inputAsArray.length) {
     let calories: number[] = []
-    inputAsArray.forEach(inputLine => {
+    inputAsArray.forEach((inputLine) => {
       if (+inputLine) {
         calories.push(+inputLine)
       } else {
-        result.push({calories, totalCalories: calories.reduce((sum, current) => sum + current)})
+        result.push({
+          calories,
+          totalCalories: calories.reduce((sum, current) => sum + current),
+        })
         calories = []
       }
-    });
+    })
     if (calories.length > 0) {
-      result.push({calories, totalCalories: calories.reduce((sum, current) => sum + current)})
+      result.push({
+        calories,
+        totalCalories: calories.reduce((sum, current) => sum + current),
+      })
       calories = []
     }
   }
@@ -31,12 +32,19 @@ export function convertInputToElves(input: string): Elf[] {
   return result
 }
 
-export function findElfWithMostCalories(elves: Elf[]): ElvesWithMaxTotalCalories[] {
+export type ElvesWithMaxTotalCalories = {
+  elfNumber: number
+  elf: Elf
+}
+
+export function findElvesWithMaxCalories(
+  elves: Elf[]
+): ElvesWithMaxTotalCalories[] {
   const result: ElvesWithMaxTotalCalories[] = []
   if (elves.length) {
-    const maxTotalCalories = Math.max(...elves.map(e => e.totalCalories))
+    const maxTotalCalories = Math.max(...elves.map((e) => e.totalCalories))
     for (let index = 0; index < elves.length; index++) {
-      const elf = elves[index];
+      const elf = elves[index]
       if (maxTotalCalories === elves[index].totalCalories) {
         result.push({elfNumber: index + 1, elf})
       }
@@ -45,15 +53,20 @@ export function findElfWithMostCalories(elves: Elf[]): ElvesWithMaxTotalCalories
   return result
 }
 
-export function findTopThreeElvesWithMostCalories(elves: Elf[]): ElvesWithMaxTotalCalories[] {
+export function findTopThreeElvesWithMostCalories(
+  elves: Elf[]
+): ElvesWithMaxTotalCalories[] {
   const result: ElvesWithMaxTotalCalories[] = []
   if (elves.length) {
-    const sortedElves = [...elves].sort((a, b) => b.totalCalories - a.totalCalories)
+    const sortedElves = [...elves].sort(
+      (a, b) => b.totalCalories - a.totalCalories
+    )
     const topThreeElves = sortedElves.slice(0, 3)
-    topThreeElves.forEach(elf => {
-      const elfNumber = elves.findIndex(e => e.totalCalories === elf.totalCalories) + 1
+    topThreeElves.forEach((elf) => {
+      const elfNumber =
+        elves.findIndex((e) => e.totalCalories === elf.totalCalories) + 1
       result.push({elfNumber, elf})
-    });
+    })
   }
   return result
 }
