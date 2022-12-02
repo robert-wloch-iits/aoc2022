4c4374a2fcebd8b0daae3765fbfb98bf5e14bad4
Robert Wloch <robert.wloch@iits-consulting.de>
2022-12-02 09:02:01 +0100

    add calculation of scores

--- src/aoc2022/day02.ts
+++ src/aoc2022/day02.ts
@@ -1,7 +1,7 @@
 export enum RockPaperScissorsOpponentType {
-  A,
-  B,
-  C,
+  A = 1,
+  B = 2,
+  C = 3,
 }
 
 export enum RockPaperScissorsResponseType {
@@ -12,7 +12,33 @@ export enum RockPaperScissorsResponseType {
 
 export type Strategy = {
   choiceOfOpponent: RockPaperScissorsOpponentType,
-  response: RockPaperScissorsResponseType
+  response: RockPaperScissorsResponseType,
+  score: number,
+}
+
+export enum ScoresType {
+  LOOSE = 0,
+  DRAW = 3,
+  WIN = 6,
+}
+
+export function calculateStrategyScore(choiceOfOpponent: RockPaperScissorsOpponentType, response: RockPaperScissorsResponseType): number {
+  let result: number
+
+  const isSameChoice = +response === +choiceOfOpponent
+  const isScissorsGotBeatenByRock = response === RockPaperScissorsResponseType.Z && choiceOfOpponent === RockPaperScissorsOpponentType.A
+  const isRockBeatsScissors = response === RockPaperScissorsResponseType.X && choiceOfOpponent === RockPaperScissorsOpponentType.C
+  const isResponseIsInferior = +response < +choiceOfOpponent && !isRockBeatsScissors
+
+  if (isSameChoice) {
+    result = +response + ScoresType.DRAW
+  } else if (isScissorsGotBeatenByRock || isResponseIsInferior) {
+    result = +response + ScoresType.LOOSE
+  } else {
+    result = +response + ScoresType.WIN
+  }
+
+  return result
 }
 
 export function parseStrategies(input: string): Strategy[] {
@@ -25,7 +51,9 @@ export function parseStrategies(input: string): Strategy[] {
       const choiceOfOpponent = RockPaperScissorsOpponentType[choiceOfOpponentString as keyof typeof RockPaperScissorsOpponentType]
       const response = RockPaperScissorsResponseType[responseString as keyof typeof RockPaperScissorsResponseType]
 
-      result.push({choiceOfOpponent, response})
+      const score = calculateStrategyScore(choiceOfOpponent, response)
+
+      result.push({choiceOfOpponent, response, score})
     });
 
   }
--- tests/unit/aoc/day02.spec.ts
+++ tests/unit/aoc/day02.spec.ts
@@ -1,11 +1,33 @@
 import {describe, it, expect} from 'vitest'
 import {
   parseStrategies,
+  calculateStrategyScore,
   RockPaperScissorsOpponentType as OpponentRPS,
   RockPaperScissorsResponseType as ResponseRPS,
+  ScoresType as Score,
 } from '@/aoc2022/day02'
 
 describe('day02', () => {
+
+  describe('calculateStrategyScore', () => {
+    const testData = [
+      {choiceOfOpponent: OpponentRPS.A, response: ResponseRPS.X, expectedScore: 4},
+      {choiceOfOpponent: OpponentRPS.A, response: ResponseRPS.Y, expectedScore: 8},
+      {choiceOfOpponent: OpponentRPS.A, response: ResponseRPS.Z, expectedScore: 3},
+      {choiceOfOpponent: OpponentRPS.B, response: ResponseRPS.X, expectedScore: 1},
+      {choiceOfOpponent: OpponentRPS.B, response: ResponseRPS.Y, expectedScore: 5},
+      {choiceOfOpponent: OpponentRPS.B, response: ResponseRPS.Z, expectedScore: 9},
+      {choiceOfOpponent: OpponentRPS.C, response: ResponseRPS.X, expectedScore: 7},
+      {choiceOfOpponent: OpponentRPS.C, response: ResponseRPS.Y, expectedScore: 2},
+      {choiceOfOpponent: OpponentRPS.C, response: ResponseRPS.Z, expectedScore: 6},
+    ]
+
+    it.each(testData)('gets $choiceOfOpponent, $response as input and returns $expectedScore', ({choiceOfOpponent, response, expectedScore}) => {
+      const result = calculateStrategyScore(choiceOfOpponent, response)
+      expect(result).toBe(expectedScore)
+    })
+  })
+
   describe('parseStrategies', () => {
     it('gets an empty list as input and returns no strategies', () => {
       const result = parseStrategies(``)
@@ -16,7 +38,7 @@ describe('day02', () => {
     it('gets a list with one entry as input and returns one strategy', () => {
       const result = parseStrategies(`A Y`)
       expect(result.length).toBe(1)
-      expect(result).toStrictEqual([{choiceOfOpponent: OpponentRPS.A, response: ResponseRPS.Y}])
+      expect(result).toStrictEqual([{choiceOfOpponent: OpponentRPS.A, response: ResponseRPS.Y, score: calculateStrategyScore(OpponentRPS.A, ResponseRPS.Y)}])
     })
 
     it('gets a list with three entries as input and returns three strategies', () => {
@@ -25,9 +47,9 @@ describe('day02', () => {
       C Z`)
       expect(result.length).toBe(3)
       expect(result).toStrictEqual([
-        {choiceOfOpponent: OpponentRPS.A, response: ResponseRPS.Y},
-        {choiceOfOpponent: OpponentRPS.B, response: ResponseRPS.X},
-        {choiceOfOpponent: OpponentRPS.C, response: ResponseRPS.Z},
+        {choiceOfOpponent: OpponentRPS.A, response: ResponseRPS.Y, score: calculateStrategyScore(OpponentRPS.A, ResponseRPS.Y)},
+        {choiceOfOpponent: OpponentRPS.B, response: ResponseRPS.X, score: calculateStrategyScore(OpponentRPS.B, ResponseRPS.X)},
+        {choiceOfOpponent: OpponentRPS.C, response: ResponseRPS.Z, score: calculateStrategyScore(OpponentRPS.C, ResponseRPS.Z)},
       ])
     })
   })
