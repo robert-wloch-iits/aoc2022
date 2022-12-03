6e3338b88423a81d3f6ae7b4f974d7409596761a
Robert Wloch <robert.wloch@iits-consulting.de>
2022-12-03 07:01:41 +0100

    split input into rucksack compartments

--- src/aoc2022/day03.ts
+++ src/aoc2022/day03.ts
@@ -1,9 +1,25 @@
 interface Rucksack {
-  compartments: string[]
+  compartments: string[][]
+}
+
+export function splitInTwoHalves(input: string[]): string[][] {
+  let result: string[][] = []
+  if (input?.length > 0) {
+    const middleIndex = Math.ceil(input.length / 2)
+    const firstHalf = input.splice(0, middleIndex)
+    const secondHalf = input.splice(-middleIndex)
+    result = [firstHalf, secondHalf]
+  }
+  return result
 }
 
 export function parseInput(input: string): Rucksack[] {
   const inputAsArray = input?.trim() ? input.trim().split('\n') : []
   const result: Rucksack[] = []
+  if (inputAsArray?.length > 0) {
+    inputAsArray.forEach(contentsAsString => {
+      result.push({compartments: splitInTwoHalves([...contentsAsString])})
+    });
+  }
   return result
 }
--- tests/unit/aoc/day03.spec.ts
+++ tests/unit/aoc/day03.spec.ts
@@ -1,12 +1,38 @@
 import {describe, it, expect} from 'vitest'
-import {parseInput} from '@/aoc2022/day03'
+import {splitInTwoHalves, parseInput, Rucksack} from '@/aoc2022/day03'
 
 describe('day03', () => {
-  describe('parseInput', () => {
-    it('gets an empty list as input and returns no rucksacks', () => {
-      const result = parseInput('')
+  describe('splitInTwoHalves', () => {
+    it('gets an empty array and returns an empty array', () => {
+      const result = splitInTwoHalves([])
       expect(result.length).toBe(0)
       expect(result).toStrictEqual([])
+    })
+
+    it('gets an array with of a string of equal number of characters and returns an array with two entries of equal size', () => {
+      const input = [...`vJrwpWtwJgWrhcsFMMfFFhFp`]
+      const expectedFirstHalf = [...`vJrwpWtwJgWr`]
+      const expectedSecondHalf = [...`hcsFMMfFFhFp`]
+      const result = splitInTwoHalves(input)
+      expect(result.length).toBe(2)
+      expect(result).toStrictEqual([expectedFirstHalf,expectedSecondHalf])
+    })
+  })
+
+  describe('parseInput', () => {
+    it('gets an empty list as input and returns no rucksacks', () => {
+      const result: Rucksack[] = parseInput('')
+      expect(result.length).toBe(0)
+      expect(result).toStrictEqual([])
+    })
+
+    it('gets a list with one entry as input and returns one rucksacks', () => {
+      const input = `vJrwpWtwJgWrhcsFMMfFFhFp`
+      const expectedFirstHalf = [...`vJrwpWtwJgWr`]
+      const expectedSecondHalf = [...`hcsFMMfFFhFp`]
+      const result: Rucksack[] = parseInput(input)
+      expect(result.length).toBe(1)
+      expect(result).toStrictEqual([{compartments: [expectedFirstHalf,expectedSecondHalf]}])
     })
   })
 })
