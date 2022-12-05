61964796a498873a51696ec4bd4bdbccc5e1593a
Robert Wloch <robert.wloch@iits-consulting.de>
2022-12-05 08:26:11 +0100

    add transpose reversed lines util

--- src/aoc2022/utils.ts
+++ src/aoc2022/utils.ts
@@ -8,3 +8,27 @@ export function parseInput<C>(input: string, typeFactoryFn: (chunk: string) => C
   }
   return result
 }
+
+export function parseInputWithoutTrim<C>(input: string, typeFactoryFn: (chunk: string) => C, delimiter = '\n'): C[] {
+  const inputChunksArray = input ? input.split(delimiter) : []
+  const result: C[] = []
+  if (inputChunksArray?.length > 0) {
+    inputChunksArray.forEach(chunkAsString => {
+      result.push(typeFactoryFn(chunkAsString))
+    });
+  }
+  return result
+}
+
+export function transposeReversedLines(input: string, columnWidth = 4, columnMarker = '|'): string {
+  let result = input
+  const reversedLines = parseInputWithoutTrim<string>(input, (chunk) => chunk).reverse()
+  if (reversedLines?.length > 0) {
+    const reversedLinesWithColumnMarker = reversedLines.map((line) => [...line].map((character,i) => (i + 1) % columnWidth ? character : columnMarker).join(''))
+    const valuesMatrix = reversedLinesWithColumnMarker.map((line) => parseInputWithoutTrim<string>(line, (chunk) => chunk, columnMarker))
+    const transposedValuesMatrix = valuesMatrix[0].map((_, c) => valuesMatrix.map((_, r) => valuesMatrix[r][c]))
+    const transposedReversedLines = transposedValuesMatrix.map((row) => row.join(columnMarker))
+    result = transposedReversedLines.join('\n')
+  }
+  return result
+}
--- tests/unit/aoc2022/utils.spec.ts
+++ tests/unit/aoc2022/utils.spec.ts
@@ -1,5 +1,5 @@
 import {describe, it, expect} from 'vitest'
-import {parseInput} from '@/aoc2022/utils'
+import {parseInput, parseInputWithoutTrim, transposeReversedLines} from '@/aoc2022/utils'
 
 type TestType = {
   value: string
@@ -40,6 +40,88 @@ bar`
       const result = parseInput(input, createTestType, '\n\n')
       expect(result.length).toBe(2)
       expect(result).toStrictEqual([{value: 'foo\noof'}, {value: 'bar'}])
+    })
+  })
+
+  describe('parseInputWithoutTrim()', () => {
+    it('gets an empty input list and returns an empty array', () => {
+      const input = ``
+      const result = parseInputWithoutTrim(input, createTestType)
+      expect(result.length).toBe(0)
+      expect(result).toStrictEqual([])
+    })
+
+    it('gets a list with one line of text and returns one TestType', () => {
+      const input = ` foo `
+      const result = parseInputWithoutTrim(input, createTestType)
+      expect(result.length).toBe(1)
+      expect(result).toStrictEqual([{value: ' foo '}])
+    })
+
+    it('gets a list with two lines of text and returns two TestTypes', () => {
+      const input = `foo 
+ bar `
+      const result = parseInputWithoutTrim(input, createTestType)
+      expect(result.length).toBe(2)
+      expect(result).toStrictEqual([{value: 'foo '}, {value: ' bar '}])
+    })
+
+    it('gets a list with two chunks of text separated by two linebreaks and returns two TestTypes', () => {
+      const input = ` foo
+ oof
+
+bar `
+      const result = parseInputWithoutTrim(input, createTestType, '\n\n')
+      expect(result.length).toBe(2)
+      expect(result).toStrictEqual([{value: ' foo\n oof'}, {value: 'bar '}])
+    })
+  })
+
+  describe('transposeReversedLines', () => {
+    it('gets an empty string as input and returns an empty string', () => {
+      const input = ``
+      const result = transposeReversedLines(input)
+      const expected = ``
+      expect(result).toBe(expected)
+    })
+
+    it('gets one line with one column as input and returns one line with the column value in that line', () => {
+      const input = `[A]`
+      const result = transposeReversedLines(input)
+      const expected = `[A]`
+      expect(result).toBe(expected)
+    })
+
+    it('gets three lines with one column as input and returns one line with three column values where the last column value is first with 2 character wide columns', () => {
+      const input = `A
+B
+C`
+      const result = transposeReversedLines(input)
+      const expected = `C|B|A`
+      expect(result).toBe(expected)
+    })
+
+    it('gets three lines with two columns as input and returns two lines with three column values where the last column\'s values are first using space as column marker', () => {
+      const input = `[A] [X]
+[B] [Y]
+[C] [Z]`
+      const result = transposeReversedLines(input, 4, ' ')
+      const expected = `[C] [B] [A]
+[Z] [Y] [X]`
+      expect(result).toBe(expected)
+    })
+
+    it('gets four lines with three columns as input and returns three lines with four column values where the last column\'s values are first', () => {
+      const input = 
+`    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3`
+      const result = transposeReversedLines(input)
+      const expected = ` 1 |[Z]|[N]|   
+ 2 |[M]|[C]|[D]
+ 3|[P]|   |   `
+      expect(result).toBe(expected)
     })
   })
 })
